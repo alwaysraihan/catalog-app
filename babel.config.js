9188581cb99e04a0e3345c17ab1db651a47aa87a
Raihan Sarkar <83104019+alwaysraihan@users.noreply.github.com>
2024-11-11 13:25:42 +0600

    ci: regular packages and folder

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src/'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '*': '.',
+          '@FoodMamaUi': './src/presentation/ui',
+          '@FoodMamaApplication': './src/application',
+          '@FoodMamaAsset': './src/presentation/assets',
+        },
+      },
+    ],
+    ['react-native-reanimated/plugin'],
+  ],
 };
