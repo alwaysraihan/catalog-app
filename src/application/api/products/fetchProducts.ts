c61afd0e6b83a0b77e672d06dcc491f9a85b0a01
alwaysraihan <bdyoungdeveloper@gmail.com>
2024-11-13 08:46:13 +0600

    fix: product fetch

--- src/application/api/products/fetchProducts.ts
+++ src/application/api/products/fetchProducts.ts
@@ -1,5 +1,5 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
-import {BASE_URL} from '@FoodMamaApplication';
+import {BASE_URL,localStorage} from '@FoodMamaApplication';
 
 const productFetchApi = createApi({
   reducerPath: 'productFetchApi',
